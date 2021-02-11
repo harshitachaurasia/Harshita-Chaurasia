003737e951a46488024a800812cb4f6b80ff7f63
Harshita Chaurasia <54536607+harshitachaurasia@users.noreply.github.com>
2021-02-11 11:31:47 +0530

    Update add_with_1_function.c
    formatted code

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main(){
 	int num1,num2,sum;
-printf("Enter first number:\n");
-scanf("%d",&num1);
-printf("Enter second number:\n");
-scanf("%d",&num2);
-sum = num1 + num2;
-printf("Sum of %d and %d is %d",num1,num2,sum);
-return 0;
+	printf("Enter first number:\n");
+	scanf("%d",&num1);
+	printf("Enter second number:\n");
+	scanf("%d",&num2);
+	sum = num1 + num2;
+	printf("Sum of %d and %d is %d",num1,num2,sum);
+	return 0;
 	
 }
