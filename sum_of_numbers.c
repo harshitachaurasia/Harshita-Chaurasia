30babb06ded28682d0c3165322b79259180ba038
Harshita Chaurasia <54536607+harshitachaurasia@users.noreply.github.com>
2021-02-11 12:06:32 +0530

    Update sum_of_numbers.c
    initial commit

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,19 @@
-//Write a program to find the sum of n different numbers using 4 functions
+//Write a program to find the sum of n different numbers 
+#include <stdio.h>
+int main() 
+{
+    int A[10],sum=0,n;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+A[i];
+    }
+    printf("The sum is: %d",sum);
+    return 0;
+}
