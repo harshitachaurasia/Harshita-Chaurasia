7b360d76e1180913def8c2508064f4b32f638200
Harshita Chaurasia <54536607+harshitachaurasia@users.noreply.github.com>
2021-02-11 11:42:59 +0530

    Update distance.c
    initial commit

--- distance.c
+++ distance.c
@@ -1,1 +1,32 @@
 //WAP to find the distance between two point using 4 functions.
+#include <stdio.h>
+#include <math.h>
+float input()
+{
+    int a;
+    scanf("%d",&a);
+    return a;
+}
+float cumpute_add(float a, float b, float c,float d)
+{
+    
+    return sqrt(pow((a-c),2)+pow((b-d),2));
+}
+int display(float sum)
+{
+    printf("the distance is:%.2f",sum);
+    return 0;
+}
+int main() 
+{
+    float a,b,c,d,sum;
+    printf("Enter (x1,y1):");
+    a=input();
+    b=input();
+    printf("Enter (x2,y2):");
+    c=input();
+    d=input();
+    sum=add(a,b,c,d);
+    display(sum);
+    return 0;
+}
