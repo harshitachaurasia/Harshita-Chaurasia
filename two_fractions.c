c09b03224093285552dd49a9557254908d80e883
Harshita Chaurasia <54536607+harshitachaurasia@users.noreply.github.com>
2021-02-17 11:50:37 +0530

    initial commit

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,42 @@
 //WAP to find the sum of two fractions.
+#include <stdio.h>
+#include <math.h>
+struct fraction{
+    int num, den;
+};
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b%a, a); 
+} 
+
+void lowest(int den3, int num3) 
+{ 
+
+    int common_factor = gcd(num3,den3); 
+    den3 = den3/common_factor; 
+    num3 = num3/common_factor; 
+} 
+
+struct fraction get_prams()
+{
+    struct fraction p1;
+    printf("\nEnter the fraction(num,den): ");
+    scanf("%d %d",&p1.num,&p1.den);
+    return p1;
+}
+
+int main(){
+    struct fraction c1,c2;
+    int num3,den3;
+    c1=get_prams();
+    c2=get_prams();
+    den3=gcd(c1.den,c2.den);
+    den3 = (c1.den*c2.den) / den3;
+    num3 = (c1.num)*(den3/c1.den) + (c2.num)*(den3/c2.den);
+    lowest(den3,num3);
+    printf("\n Sum of %d/%d and %d/%d is : %d/%d \n",c1.num,c1.den,c2.num,c2.den,num3,den3);
+    return 0;
+}
